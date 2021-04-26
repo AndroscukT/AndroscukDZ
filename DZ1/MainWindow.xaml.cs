0392662ad94e593b48589c7fd8dd13c122af5520
mggtkagu <mggtkagu@mail.ru>
2021-04-26 15:12:24 +0300

    DZ1

---
+++ DZ1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DZ1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String login = LoginTextBox.Text;
+            String password = PasswordBox.Password;
+
+            if (login!= "login1" || password!= "tuptup")
+            {
+                MessageBox.Show("Проверьте введеные вами данные", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else {
+                
+               Information information = new Information();
+               information.Show();
+               this.Close();
+            }
+        }
+
+        private void LoginTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (PCheckBox.IsChecked == true)
+            {
+                PasswordBox.Visibility = Visibility.Hidden;
+                PasswordTextBox.Visibility = Visibility.Visible;
+                PasswordTextBox.Text = PasswordBox.Password;
+            }
+        }
+        private void PCheckBox_UnChecked(object sender, RoutedEventArgs e)
+        {     
+                PasswordBox.Visibility = Visibility.Visible;
+                PasswordTextBox.Visibility = Visibility.Hidden;             
+                PasswordBox.Password = PasswordTextBox.Text;             
+        }
+
+    }
+}
---
+++ DZ1/SaleClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    public class SaleClass
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public int Kol { get; set; }
+        public double Price { get; set; }
+    }
+}
